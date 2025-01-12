0c60e1aaac4aa36ac95d6d77eaef0825aca2fec9
vanshaj Tiwari <vanshajtiwari62@gmail.com>
2025-01-13 01:48:09 +0530

    done

--- server/src/middlewares/cors.ts
+++ server/src/middlewares/cors.ts
@@ -1,23 +1,32 @@
 import { Request, Response, NextFunction } from "express";
 
 type CORS = (req: Request, res: Response, next: NextFunction) => void;
- 
+
 let allowedHeaders = ["Authorization", "Content-Type"];
 
 const cors: CORS = (req, res, next) => {
-  let origin = ['http://localhost:3000', process.env.REMOTE_FRONTEND_URL];
-  let method = req.method;
+  const allowedOrigins = [
+    "http://localhost:3000",
+    process.env.REMOTE_FRONTEND_URL || "",
+  ];
 
-  if (origin) {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "*");
+  const origin = req.headers.origin;
+
+  if (allowedOrigins.includes(origin as string)) {
+    res.setHeader("Access-Control-Allow-Origin", origin??"");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     res.setHeader("Access-Control-Allow-Headers", allowedHeaders.join(", "));
     res.setHeader("Access-Control-Allow-Credentials", "true");
 
-    return method == "OPTIONS" ? res.status(200).end() : next();
+    if (req.method === "OPTIONS") {
+      return res.status(200).end();
+    }
+
+    return next();
   }
 
-  return !origin ? next() : res.status(403).end();
+  // If the origin is not allowed, return 403
+  return res.status(403).end();
 };
 
 export default cors;
