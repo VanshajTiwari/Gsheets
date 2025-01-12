0ffa4986f1b77a1faaadb4cf432da090b8940b36
vanshaj Tiwari <vanshajtiwari62@gmail.com>
2025-01-13 02:11:46 +0530

    done

--- server/src/middlewares/cors.ts
+++ server/src/middlewares/cors.ts
@@ -2,33 +2,25 @@ import { Request, Response, NextFunction } from "express";
 
 type CORS = (req: Request, res: Response, next: NextFunction) => void;
 
+let regex =
+  /^(?:https?:\/\/(?:vanshajtiwari\.github\.io|localhost:\d+|vercel\.com))$/;
+
 let allowedHeaders = ["Authorization", "Content-Type"];
 
 const cors: CORS = (req, res, next) => {
-  const allowedOrigins = [
-    "http://localhost:3000",
-    "https://gsheets-mnf4-git-main-vanshajtiwaris-projects.vercel.app",
-    "https://gsheets-mnf4.vercel.app",
-    "https://gsheets-mnf4-vanshajtiwaris-projects.vercel.app/#/auth/sign-in"
-  ];
+  let origin = req.headers.origin;
+  let method = req.method;
 
-  const origin = req.headers.origin;
-
-  if (allowedOrigins.includes(origin as string)) {
-    res.setHeader("Access-Control-Allow-Origin", origin??"");
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  if (origin && regex.test(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "*");
     res.setHeader("Access-Control-Allow-Headers", allowedHeaders.join(", "));
     res.setHeader("Access-Control-Allow-Credentials", "true");
 
-    if (req.method === "OPTIONS") {
-      return res.status(200).end();
-    }
-
-    return next();
+    return method == "OPTIONS" ? res.status(200).end() : next();
   }
 
-  // If the origin is not allowed, return 403
-  return res.status(403).end();
+  return !origin ? next() : res.status(403).end();
 };
 
 export default cors;
